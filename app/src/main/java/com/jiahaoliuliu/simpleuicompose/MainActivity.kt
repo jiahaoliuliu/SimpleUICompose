a6f86bf77c29c8c3b97ac30dc1be45299a648373
Jiahao Liu <jiahao.liu@dnata.com>
2021-11-09 11:55:21 +0400

    Created the first composable method

--- app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard(name = "Android")
         }
+    }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name!")
     }
 }

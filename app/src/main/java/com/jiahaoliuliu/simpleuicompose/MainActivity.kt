54c509d7f2380a6207f620c01a04344872a89760
Jiahao Liu <jiahao.liu@dnata.com>
2021-11-09 11:29:16 +0400

    First commit

---
+++ app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jiahaoliuliu.simpleuicompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

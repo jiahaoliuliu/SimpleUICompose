8cc67f081318b9699309a06073bf7eca3ede5282
Jiahao Liu <jiahao.liu@dnata.com>
2021-11-09 11:57:55 +0400

    Added a preview composable method

--- app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +20,11 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun MessageCard(name: String) {
         Text(text = "Hello $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(name = "Android")
     }
 }

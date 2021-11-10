cc0835e9a3bc4055db9df4ae6bd0c28e33308640
Jiahao Liu <jiahao.liu@dnata.com>
2021-11-10 10:24:06 +0400

    Previewed the dark theme

--- app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.jiahaoliuliu.simpleuicompose
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,7 +25,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            MessageCard(Message("Android", "Jetpack Compose"))
             ComposeTutorialTheme {
                 MessageCard(Message("Android", "Jetpack Compose"))
             }
@@ -30,13 +33,6 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun MessageCard(msg: Message) {
-//        Row {
-//            Image(
-//                painter = painterResource(id = R.drawable.photo),
-//                contentDescription = "Contact profile picture"
-//            )
-//        }
-
         // Add padding around our message
         Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
@@ -47,20 +43,37 @@ class MainActivity : AppCompatActivity() {
                     .size(40.dp)
                     // Clip image to be shaped as a circle
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
 
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
             Column {
-                Text(text = msg.author)
+                Text(text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2)
                 // Add a vertical space between the author and message texts
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = msg.body)
+
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2)
+                }
             }
         }
     }
 
-    @Preview
+    @Preview(
+        name = "Light Mode",
+        showBackground = true)
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+
+    )
     @Composable
     fun PreviewMessageCard() {
         ComposeTutorialTheme {

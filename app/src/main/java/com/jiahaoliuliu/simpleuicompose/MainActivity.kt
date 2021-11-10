fa4462affae8801d72fe1bb15a712b52e4d80ec1
Jiahao Liu <jiahao.liu@dnata.com>
2021-11-10 11:37:28 +0400

    Updated the libraries and showing the converstion messages. Now the messages are expandible

--- app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/simpleuicompose/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,7 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -50,7 +51,12 @@ class MainActivity : AppCompatActivity() {
 
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
-            Column {
+
+            // We keep track if the message is expanded or not in this variable
+            var isExpanded: Boolean by remember { mutableStateOf(false) }
+
+            // We toggle the isExpanded variable when we click on this column
+            Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
                 Text(text = msg.author,
                     color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.subtitle2)
@@ -61,6 +67,9 @@ class MainActivity : AppCompatActivity() {
                     Text(
                         text = msg.body,
                         modifier = Modifier.padding(all = 4.dp),
+                        // if the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         style = MaterialTheme.typography.body2)
                 }
             }
